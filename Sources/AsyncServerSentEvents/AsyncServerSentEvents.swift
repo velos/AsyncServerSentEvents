2e96086a419688d3349952289a3fe358477fa263
Zac White <zac@velosmobile.com>
2026-01-28 22:39:54 -0800

    More changes for CI

--- Sources/AsyncServerSentEvents/AsyncServerSentEvents.swift
+++ Sources/AsyncServerSentEvents/AsyncServerSentEvents.swift
@@ -7,10 +7,10 @@ public struct AsyncServerSentEvents: AsyncSequence {
     typealias Continuation = AsyncStream<Element>.Continuation
 
     public struct Event: Hashable, Sendable, Equatable {
-        var id: String?
-        var name: String?
-        var comment: String?
-        var data: String
+        public var id: String?
+        public var name: String?
+        public var comment: String?
+        public var data: String
 
         mutating func appending(commentLine: String) {
             if comment != nil  {
@@ -37,66 +37,70 @@ public struct AsyncServerSentEvents: AsyncSequence {
         (stream, continuation) = AsyncStream<Element>.makeStream()
 
         Task { [continuation] in
-            let lines = try await bytes
-                .split({ elements in
-                    if elements == [10, 13] {
-                        return [10, 13]
-                    } else if elements.first == 10 {
-                        return [10]
-                    } else if elements.first == 13 {
-                        return [13]
-                    }
-                    return nil
-                }, window: 2, omittingEmptySubsequences: false)
-                .compactMap { String(bytes: $0, encoding: .utf8) }
+            do {
+                let lines = try await bytes
+                    .split({ elements in
+                        if elements == [10, 13] {
+                            return [10, 13]
+                        } else if elements.first == 10 {
+                            return [10]
+                        } else if elements.first == 13 {
+                            return [13]
+                        }
+                        return nil
+                    }, window: 2, omittingEmptySubsequences: false)
+                    .compactMap { String(bytes: $0, encoding: .utf8) }
 
-            var event = Event(data: "")
-            for try await line in lines {
-                if line.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
-                    if event != Event(data: "") {
-                        event.trim()
-                        continuation.yield(event)
-                    }
-                    event = Event(data: "")
-                } else {
-                    var elements = line.split(separator: ":", maxSplits: 1, omittingEmptySubsequences: false)
-                    if elements.count == 1 {
-                        elements.append("")
-                    }
+                var event = Event(data: "")
+                for try await line in lines {
+                    if line.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+                        if event != Event(data: "") {
+                            event.trim()
+                            continuation.yield(event)
+                        }
+                        event = Event(data: "")
+                    } else {
+                        var elements = line.split(separator: ":", maxSplits: 1, omittingEmptySubsequences: false)
+                        if elements.count == 1 {
+                            elements.append("")
+                        }
 
-                    let field = elements[0]
-                        .trimmingCharacters(in: .whitespaces)
-                        .lowercased()
-                    let value = String(
-                        elements[1]
+                        let field = elements[0]
                             .trimmingCharacters(in: .whitespaces)
-                    )
+                            .lowercased()
+                        let value = String(
+                            elements[1]
+                                .trimmingCharacters(in: .whitespaces)
+                        )
 
-                    switch field {
-                    case "id":
-                        if !value.isEmpty {
-                            event.id = value
+                        switch field {
+                        case "id":
+                            if !value.isEmpty {
+                                event.id = value
+                            }
+                        case "event":
+                            if !value.isEmpty {
+                                event.name = value
+                            }
+                        case "":
+                            event.appending(commentLine: value)
+                        case "data":
+                            event.appending(dataLine: value)
+                        default:
+                            continue
                         }
-                    case "event":
-                        if !value.isEmpty {
-                            event.name = value
-                        }
-                    case "":
-                        event.appending(commentLine: value)
-                    case "data":
-                        event.appending(dataLine: value)
-                    default:
-                        continue
                     }
                 }
-            }
 
-            if event != Event(data: "") {
-                event.trim()
-                continuation.yield(event)
-            }
+                if event != Event(data: "") {
+                    event.trim()
+                    continuation.yield(event)
+                }
 
-            continuation.finish()
+                continuation.finish()
+            } catch {
+                print("ERROR: \(error)")
+            }
         }
     }
 
